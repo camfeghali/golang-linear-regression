675d16208bdf23de555702fd3fa6bdbc19ebd337
camfeghali <camfeg@gmail.com>
2022-12-24 18:05:23 +0200

    gradient descent passes tests

--- linreg.go
+++ linreg.go
@@ -16,7 +16,7 @@ func (model *LinearRegression) Train(features, targets []float64) (float64, floa
 	// b_init: 0, 
 	// alpha: 0.001, 
 	// num_iterations: 1000
-	w_final, b_final, J_hist, p_hist := SingleVarGradientDescent(features, targets, 0, 0, 0.001, 1000)
+	w_final, b_final, J_hist, p_hist := SingleVarGradientDescent(features, targets, 0, 0, 0.01, 10000)
 
 	model.W = w_final
 	model.B = b_final
@@ -32,64 +32,10 @@ func (model *LinearRegression) Predict(x float64) float64 {
 	return SingleVarPrediction(x, model.W, model.B)
 }
 
-func MultiVarPrediction(x, w []float64, b float64) float64 {
-	dotProduct, _ := dotProduct(w, x)
-	total_sum := reduce(dotProduct, func(acc, current float64) float64 {
-        return acc + current
-    }, 0)
-	prediction := total_sum + b
-	return prediction
-}
-
 func SingleVarPrediction(x, w, b float64) float64 {
 	return w*x + b
 }
 
-
-func SingleVarGradientDescent(x, y []float64, w_init, b_init, alpha float64, num_iterations int) (float64, float64, []float64, [][]float64){
-    // Performs gradient descent to fit w,b. Updates w,b by taking 
-    // num_iters gradient steps with learning rate alpha
-    
-    // Args:
-    //   x					: Data, m examples 
-    //   y					: target values
-    //   w_in,b_in			: initial values of model parameters  
-    //   alpha				: Learning rate
-    //   num_iters			: number of iterations to run gradient descent
-    //   cost_function		: function to call to produce cost
-    //   gradient_function	: function to call to produce gradient
-      
-    // Returns:
-    //   w					: Updated value of parameter after running gradient descent
-    //   b 					: Updated value of parameter after running gradient descent
-    //   J_history (List)	: History of cost values
-    //   p_history (list)	: History of parameters [w,b] 
-
-	J_history := make([]float64, 0)
-	parameter_history := make([][]float64, 0)
-	b := b_init
-	w := w_init
-
-	for idx := range x {
-		dj_dw, dj_db := compute_gradient_single_var(x, y, b, w)
-
-		b = b - alpha*dj_db
-		w = w - alpha*dj_dw
-
-		if idx < 1000 {
-			J_history = append(J_history, compute_cost_single_var(x, y, w, b))
-			parameter_history = append(parameter_history, []float64{w, b})
-		}
-	}
-	return w, b, J_history, parameter_history
-}
-
-// func compute_cost_multi_var(w, x[][]float64, y[]float64, b float64) float64 {
-// 	m := float64(len(x))
-// 	cost_sum := 0.0
-// }
-
-// Mean Square Error Cost Function
 func compute_cost_single_var(x, y []float64, w, b float64) float64 {
     //	Computes the cost function for linear regression.
     //	Args:
@@ -110,6 +56,29 @@ func compute_cost_single_var(x, y []float64, w, b float64) float64 {
 		total_cost := (1/(2*m)) * cost_sum
 		return total_cost
 }
+
+
+func SingleVarGradientDescent(x, y []float64, w_init, b_init, alpha float64, num_iterations int) (float64, float64, []float64, [][]float64) {
+	J_history := make([]float64, 0)
+	parameter_history := make([][]float64, 0)
+	b := b_init
+	w := w_init
+
+	for idx:= 0; idx < num_iterations; idx ++ {
+		dj_dw, dj_db := compute_gradient_single_var(x, y, w, b)
+
+		b = b - (alpha*dj_db)
+		w = w - (alpha*dj_dw)
+
+		if idx < 100000 {
+			J_history = append(J_history, compute_cost_single_var(x, y, w, b))
+			parameter_history = append(parameter_history, []float64{w, b})
+		}
+	}
+	return w, b, J_history, parameter_history
+}
+
+// Mean Square Error Cost Function
 
 func compute_gradient_single_var(x []float64, y []float64, w float64, b float64) (float64, float64) {
     // Computes the gradient for linear regression 
@@ -139,6 +108,46 @@ func compute_gradient_single_var(x []float64, y []float64, w float64, b float64)
 	return dj_w, dj_b
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func MultiVarPrediction(x, w []float64, b float64) float64 {
+	dotProduct, _ := dotProduct(w, x)
+	total_sum := reduce(dotProduct, func(acc, current float64) float64 {
+        return acc + current
+    }, 0)
+	prediction := total_sum + b
+	return prediction
+}
+
 func dotProduct(v1, v2 []float64) ([]float64, error) {
 	if len(v1) != len(v2) {
 		return nil, fmt.Errorf("x and y have unequal lengths: %d / %d", len(v1), len(v2))
@@ -165,4 +174,38 @@ func reduce[T, M any](s []T, f func(M, T) M, initValue M) M {
         acc = f(acc, v)
     }
     return acc
+}
+
+func ZScoreNormalize(Xs []float64) []float64 {
+	mean := calcMean(Xs)
+
+	std := calcStdDev(Xs, mean)
+
+	return func(Xs []float64) []float64 {
+		var normalized []float64
+		for _, value := range Xs {
+			normalized = append(normalized, (value - mean) / std)
+		}
+		return normalized
+	}(Xs)
+}
+
+func calcMean(sample []float64) float64 {
+		sum := 0.0
+		for _, value := range sample {
+			sum = sum + value
+		}
+		return sum /float64(len(sample))
+}
+
+func calcStdDev(sample []float64, mean float64) float64 {
+		sum := 0.0
+		for _, value := range sample {
+			value = square(math.Abs(value - mean))
+			sum = sum + value
+		}
+
+		variance := sum / float64(len(sample) - 1)
+
+		return math.Sqrt(variance)
 }
--- linreg_test.go
+++ linreg_test.go
@@ -9,6 +9,86 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGradientDescent(t *testing.T) {
+	assert := assert.New(t)
+
+	var x_train = []float64{1.0, 2.0}
+	var y_train = []float64{300.0, 500.0}
+	w_init := 0.0
+	b_init := 0.0
+	iterations := 10000
+	tmp_alpha := 0.01
+
+	w_final, b_final, _, _ := SingleVarGradientDescent(x_train, y_train, w_init, b_init, tmp_alpha, iterations)
+
+	assert.Equal(199.99285075131766, w_final, "");
+	assert.Equal(100.011567727362, b_final, "");
+
+}
+
+func TestComputeGradient (t *testing.T) {
+	assert := assert.New(t)
+	// x: [1. 2.], y: [300. 500.], w: 0, b: 0, dj_dw: -650.0, dj_db: -400.0
+	var x_train = []float64{1.0, 2.0}
+	var y_train = []float64{300.0, 500.0}
+
+	w := 0.0
+	b := 0.0
+
+	dj_dw, dj_db := compute_gradient_single_var(x_train, y_train, w, b)
+	assert.Equal(-650.0, dj_dw, "");
+	assert.Equal(-400.0, dj_db, "");
+
+	dj_dw, dj_db = compute_gradient_single_var(x_train, y_train, 55.849817910123896, 34.345440679260854)
+	assert.Equal(-458.857294205799, dj_dw, "");
+	assert.Equal(-281.8798324555533, dj_db, "");
+
+	dj_dw, dj_db = compute_gradient_single_var(x_train, y_train, 191.23105305470705, 114.13601208201743)
+	assert.Equal(-0.718349240206237, dj_dw, "");
+	assert.Equal(0.9825916640780008, dj_db, "");
+
+		dj_dw, dj_db = compute_gradient_single_var(x_train, y_train, 199.99284553220375, 100.01157617206572)
+	assert.Equal(-0.0005219113920702512, dj_dw, "");
+	assert.Equal(0.0008444703713337276, dj_db, "");
+}
+
+func TestCostFunction(t *testing.T) {
+	assert := assert.New(t)
+
+	var x_train = []float64{1.0, 2.0}
+	var y_train = []float64{300.0, 500.0}
+
+	w := 190.0
+	b := 100.0
+
+	cost := compute_cost_single_var(x_train, y_train, w, b)
+	assert.Equal(125.0, cost, "");
+}
+
+func TestCalcStdDev(t *testing.T) {
+	assert := assert.New(t)
+
+	var sample1 = []float64{66.0, 30.0, 40.0, 64.0}
+	mean1 := calcMean(sample1)
+	result1 := calcStdDev(sample1, mean1)
+
+	var sample2 = []float64{51.0, 21.0, 79.0, 49.0}
+	mean2 := calcMean(sample2)
+	result2 := calcStdDev(sample2, mean2)
+
+	assert.Equal(17.813852287849848, result1, "");
+	assert.Equal(23.692474191889147, result2, "");
+}
+
+
+func TestCalcMean(t *testing.T) {
+	var sample = []float64{66.0, 30.0, 40.0, 64.0}
+
+	assert := assert.New(t)
+	result := calcMean(sample)
+	assert.Equal(50.0, result, "");
+}
+
 
 func TestDot(t *testing.T) {
 	var v1 []float64 = randFloats(1.0, 100.0, 1000000)
